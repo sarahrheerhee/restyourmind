0a8f1a8e01de5bb4528cd127d4e7801370d5d40d
sarahrheerhee <36961253+sarahrheerhee@users.noreply.github.com>
2020-03-22 15:39:36 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+console.log('hi');
+
+$(document).ready(function() {
+    // javascript goes here
+
+$('#first').mouseover( function () {
+
+   $(".danae").addClass("danae2");
+
+});
+
+
+
+
+$('#first').mouseleave( function () {
+
+   $(".danae").removeClass("danae2");
+
+});
+
+
+
+
+$('#second').mouseover( function () {
+
+   $(".mile").addClass("mile2");
+
+});
+
+
+
+
+$('#second').mouseleave( function () {
+
+   $(".mile").removeClass("mile2");
+
+});
+
+
+
+
+$('#third').mouseover( function () {
+
+   $(".catso").addClass("catso2");
+
+});
+
+
+
+
+$('#third').mouseleave( function () {
+
+   $(".catso").removeClass("catso2");
+
+});
+
+
+$('#fourth').mouseover( function () {
+
+   $(".untitled").addClass("untitled2");
+
+});
+
+
+
+
+$('#fourth').mouseleave( function () {
+
+   $(".untitled").removeClass("untitled2");
+
+});
+
+
+
+
+
+
+
+});
+
