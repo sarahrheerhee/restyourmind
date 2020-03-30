e5e60d757f98004cad56d1a92d3706dbc5e31af4
sarahrheerhee <36961253+sarahrheerhee@users.noreply.github.com>
2020-03-30 10:27:49 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,17 +3,6 @@ console.log('hi');
 $(document).ready(function() {
     // javascript goes here
 
-$(function(){
-  setTimeout(function(){
-    $('html, body')
-    .css({
-      'overflow': 'auto'
-    })
-    .animate({
-      scrollTop: $('.myDiv').offset().top
-    }, 11000);
-  }, 11000);
-});
 
 
 $('#first').mouseover( function () {
@@ -79,6 +68,62 @@ $('#third').click( function () {
    window.location.href = "catso.html";
 
 });
+
+
+// $('#homemenu').mouseover( function () {
+
+//    $("#homemenu").addClass("menufill");
+
+// });
+
+
+
+// $('#homemenu').mouseleave( function () {
+
+//    $("#homemenu").removeClass("menufill");
+
+// });
+
+
+// $('#middlemenu').mouseover( function () {
+
+//    $("#middlemenu").addClass("menufill");
+
+// });
+
+
+
+// $('#middlemenu').mouseleave( function () {
+
+//    $("#middlemenu").removeClass("menufill");
+
+// });
+
+// $('#lastmenu').mouseover( function () {
+
+//    $("#lastmenu").addClass("menufill");
+
+// });
+
+// $('#lastmenu').mouseleave( function () {
+
+//    $("#lastmenu").removeClass("menufill");
+
+// });
+
+// $('#more').mouseover( function () {
+
+//    $("more").addClass("letterspacing");
+
+// });
+
+// $('#more').mouseover( function () {
+
+//    $("#more").removeClass("letterspacing");
+
+// });
+
+
 
 // $('#fourth').mouseover( function () {
 
